36944b2e1911839a19a89a71a119bbe67b7f016b
Jeffrey-Chung <Jeffreychungcl@gmail.com>
2024-08-26 18:33:53 +1000

    fix source_dir and output_path

--- lambda.tf
+++ lambda.tf
@@ -4,12 +4,12 @@ All the Configuration for Lambda goes here, the Lambda function is created in th
 
 data "archive_file" "lambda_source_code" {
   type        = "zip"
-  source_dir  = "${path.root}/lambda_src_chrome/"
-  output_path = "${path.root}/lambda_src_chrome/lambda_src_chrome.zip"
+  source_dir  = "${path.root}/aws_lambda_functions/lambda_src_chrome/"
+  output_path = "${path.root}/aws_lambda_functions/lambda_src_chrome.zip"
 }
 
 resource "aws_lambda_function" "jchung_lambda_function_chrome" {
-  filename      = "${path.root}/lambda_src_chrome/lambda_src_chrome.zip"
+  filename      = "${path.root}/aws_lambda_functions/lambda_src_chrome.zip"
   function_name = "jchung_test_uts_website_chrome"
   role          = "arn:aws:iam::663790350014:role/jchung_lambda_role"
   handler       = "index.lambda_handler"
