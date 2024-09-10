50f34fe739e861337ac0a7d5e1c96ccef18cc9c9
Jeffrey-Chung <Jeffreychungcl@gmail.com>
2024-09-10 18:43:55 +1000

    Add files to create API for firefox test

--- lambda.tf
+++ lambda.tf
@@ -8,6 +8,12 @@ data "archive_file" "lambda_source_code" {
   output_path = "${path.root}/aws_lambda_functions/lambda_src_chrome.zip"
 }
 
+data "archive_file" "lambda_source_code_firefox" {
+  type        = "zip"
+  source_dir  = "${path.root}/aws_lambda_functions/lambda_src_firefox/"
+  output_path = "${path.root}/aws_lambda_functions/lambda_src_firefox.zip"
+}
+
 resource "aws_lambda_function" "jchung_lambda_function_chrome" {
   filename      = "${path.root}/aws_lambda_functions/lambda_src_chrome.zip"
   function_name = "jchung_test_uts_website_chrome"
@@ -19,8 +25,29 @@ resource "aws_lambda_function" "jchung_lambda_function_chrome" {
   }
 }
 
+resource "aws_lambda_function" "jchung_lambda_function_firefox" {
+  filename      = "${path.root}/aws_lambda_functions/lambda_src_firefox.zip"
+  function_name = "jchung_test_uts_website_firefox"
+  role          = "arn:aws:iam::663790350014:role/jchung_lambda_role"
+  handler       = "index.lambda_handler"
+  runtime       = "python3.10"
+  tracing_config {
+    mode = "Active"
+  }
+}
+
 resource "aws_lambda_function_url" "lambda_function_url" {
   function_name      = aws_lambda_function.jchung_lambda_function_chrome.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+  }
+}
+
+resource "aws_lambda_function_url" "lambda_function_url_firefox" {
+  function_name      = aws_lambda_function.jchung_lambda_function_firefox.function_name
   authorization_type = "NONE"
 
   cors {
